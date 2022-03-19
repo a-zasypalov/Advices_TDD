047a39844143dc7f58d004f0cd4a39e7e1ee6017
Artem Zasypalov <azasypalov_ext@babbel.com>
2022-03-19 19:12:32 +0100

    Initial commit

---
+++ app/src/main/java/com/gaoyun/advices/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.gaoyun.advices
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.gaoyun.advices.ui.theme.AdvicesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AdvicesTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting(getString(R.string.app_name))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
