bc173c01eb517056833df26955492330ec1f269f
Artem Zasypalov <azasypalov_ext@babbel.com>
2022-03-19 21:02:03 +0100

    Add getRandomAdvice request implementation

--- data/src/main/java/com/gaoyun/advices/data/remote/AdvicesSlipApiClient.kt
+++ data/src/main/java/com/gaoyun/advices/data/remote/AdvicesSlipApiClient.kt
@@ -1,5 +1,12 @@
 package com.gaoyun.advices.data.remote
 
+import com.gaoyun.advices.data.model.AdviceSlipResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+
 interface AdvicesSlipApiClient {
+
+    @GET
+    fun getRandomAdvice(): Single<AdviceSlipResponse>
 
 }
