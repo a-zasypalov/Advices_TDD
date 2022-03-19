d108933f0a65945055a8aa3dde138c40e4daa646
Artem Zasypalov <azasypalov_ext@babbel.com>
2022-03-19 21:12:53 +0100

    Add AdviceRepository getRandomAdvice function

--- data/src/main/java/com/gaoyun/advices/data/repository/AdviceRepository.kt
+++ data/src/main/java/com/gaoyun/advices/data/repository/AdviceRepository.kt
@@ -1,9 +1,13 @@
 package com.gaoyun.advices.data.repository
 
+import com.gaoyun.advices.data.model.AdviceResponse
 import com.gaoyun.advices.data.remote.AdvicesSlipApiClient
+import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 interface AdviceRepository {
+
+    fun getRandomAdvice(): Single<AdviceResponse>
 
 }
 
@@ -11,5 +15,6 @@ class AdviceRepositoryImpl @Inject constructor(
     private val apiClient: AdvicesSlipApiClient
 ) : AdviceRepository {
 
+    override fun getRandomAdvice(): Single<AdviceResponse> = apiClient.getRandomAdvice().map { it.slipResponse }
 
 }
